683574304dbf43f783ebbcc335359fa876bd356e
Tim Goyer <tim.goyer@gmail.com>
2025-02-10 18:44:46 -0600

    Add useLocalStorage

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,6 @@
 export * from './useData/useData';
 export * from './useDebug/useDebug';
+export * from './useLocalStorage/useLocalStorage';
 export * from './useOutsideClick/useOutsideClick';
 export * from './usePortal/usePortal';
 export * from './useUrlParam/useUrlParam';
---
+++ lib/useLocalStorage/useLocalStorage.ts
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+
+export function useLocalStorage(key: string, initialValue: unknown) {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.error(error);
+      return initialValue;
+    }
+  });
+
+  useEffect(() => {
+    try {
+      const serializedValue = JSON.stringify(storedValue);
+      window.localStorage.setItem(key, serializedValue);
+    } catch (error) {
+      console.error(error);
+    }
+  }, [key, storedValue]);
+
+  useEffect(() => {
+    const handleStorageChange = (event: StorageEvent) => {
+      if (event.key === key) {
+        try {
+          setStoredValue(event.newValue ? JSON.parse(event.newValue) : initialValue);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    };
+
+    window.addEventListener('storage', handleStorageChange);
+    return () => window.removeEventListener('storage', handleStorageChange);
+  }, [key, initialValue]);
+
+  return [storedValue, setStoredValue];
+}
