5804b81cf7e44382ce3f0ae71676bc43d52f8adc
Tim Goyer <tim.goyer@alliancebernstein.com>
2025-04-09 13:45:29 -0500

    Replace useLocalStorage hook

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,7 @@
+// https://usehooks.com/
+export { useLocalStorage } from '@uidotdev/usehooks';
+
 export * from './useData/useData';
 export * from './useDebug/useDebug';
-export * from './useLocalStorage/useLocalStorage';
 export * from './useOutsideClick/useOutsideClick';
 export * from './useUrlParam/useUrlParam';
--- lib/useLocalStorage/useLocalStorage.ts
+++ lib/useLocalStorage/useLocalStorage.ts
@@ -1,64 +1,64 @@
-import { useCallback, useEffect, useState } from 'react';
+// import { useCallback, useEffect, useState } from 'react';
 
-const eventName = 'local-storage-change';
+// const eventName = 'local-storage-change';
 
-export function useLocalStorage(key: string, initialValue: unknown) {
-  const [item, setItem] = useState<unknown>();
+// export function useLocalStorage(key: string, initialValue: unknown) {
+//   const [item, setItem] = useState<unknown>();
 
-  const setValue = useCallback(
-    (value: unknown) => {
-      const oldV = window.localStorage.getItem(key);
-      const newV = value == null ? null : JSON.stringify(parse(value));
+//   const setValue = useCallback(
+//     (value: unknown) => {
+//       const oldV = window.localStorage.getItem(key);
+//       const newV = value == null ? null : JSON.stringify(parse(value));
 
-      if (newV == null) {
-        localStorage.removeItem(key);
-      } else if (oldV !== newV) {
-        localStorage.setItem(key, newV);
-        window.dispatchEvent(
-          new StorageEvent(eventName, {
-            key,
-            oldValue: oldV,
-            newValue: newV,
-          }),
-        );
-      }
-    },
-    [key],
-  );
+//       if (newV == null) {
+//         localStorage.removeItem(key);
+//       } else if (oldV !== newV) {
+//         localStorage.setItem(key, newV);
+//         window.dispatchEvent(
+//           new StorageEvent(eventName, {
+//             key,
+//             oldValue: oldV,
+//             newValue: newV,
+//           }),
+//         );
+//       }
+//     },
+//     [key],
+//   );
 
-  const handleLocalStorageChange = useCallback(() => {
-    setItem(getSnapshot(key));
-  }, [key]);
+//   const handleLocalStorageChange = useCallback(() => {
+//     setItem(getSnapshot(key));
+//   }, [key]);
 
-  useEffect(() => {
-    const snapshot = getSnapshot(key);
-    setValue(snapshot != null ? snapshot : initialValue);
-  }, [key, initialValue, setValue]);
+//   useEffect(() => {
+//     const snapshot = getSnapshot(key);
+//     setValue(snapshot != null ? snapshot : initialValue);
+//   }, [key, initialValue, setValue]);
 
-  useEffect(() => {
-    window.addEventListener(eventName, handleLocalStorageChange);
-    return () => {
-      window.removeEventListener(eventName, handleLocalStorageChange);
-    };
-  }, [handleLocalStorageChange]);
+//   useEffect(() => {
+//     window.addEventListener(eventName, handleLocalStorageChange);
+//     return () => {
+//       window.removeEventListener(eventName, handleLocalStorageChange);
+//     };
+//   }, [handleLocalStorageChange]);
 
-  return [item, setValue];
-}
+//   return [item, setValue];
+// }
 
-function getSnapshot(key: string) {
-  return parse(localStorage.getItem(key));
-}
+// function getSnapshot(key: string) {
+//   return parse(localStorage.getItem(key));
+// }
 
-function parse(value: unknown) {
-  const v = typeof value === 'function' ? value() : value;
-  return isJsonString(v) ? JSON.parse(v) : v;
-}
+// function parse(value: unknown) {
+//   const v = typeof value === 'function' ? value() : value;
+//   return isJsonString(v) ? JSON.parse(v) : v;
+// }
 
-function isJsonString(value: string): boolean {
-  try {
-    JSON.parse(value);
-  } catch (e) {
-    return false;
-  }
-  return true;
-}
+// function isJsonString(value: string): boolean {
+//   try {
+//     JSON.parse(value);
+//   } catch (e) {
+//     return false;
+//   }
+//   return true;
+// }
